3e1de93656d1624cdb5cc462e5b40b0c8f93e7a8
Geoffrey Lamke <glamke@gmail.com>
2019-09-23 16:30:13 -0400

    FSM spooky eyes

---
+++ src/secrets.h
@@ -0,0 +1,8 @@
+const char* ssid     = "shmoo5";
+const char* password = "s1lkw0rm";
+IPAddress ip(10, 10, 10, 173); // where xx is the desired IP Address
+IPAddress gateway(10, 10, 10, 1); // set gateway to match your network
+IPAddress subnet(255, 255, 255, 0); // set subnet mask to match your
+IPAddress dns(10, 10, 10, 1); // set subnet mask to match your
+const char* AP_NameChar = "ESP_WS";
+const char* WiFiAPPSK   = "password";
